d801264da4ea746a99be8588e8a88a10dffe1c30
Ben Kolera <ben.kolera@gmail.com>
2019-06-03 15:45:02 +1000

    Hello world

---
+++ default.nix
@@ -0,0 +1,20 @@
+{ obelisk ? import ./.obelisk/impl {
+    system = builtins.currentSystem;
+    iosSdkVersion = "10.2";
+    # You must accept the Android Software Development Kit License Agreement at
+    # https://developer.android.com/studio/terms in order to build Android apps.
+    # Uncomment and set this to `true` to indicate your acceptance:
+    # config.android_sdk.accept_license = false;
+  }
+}:
+with obelisk;
+project ./. ({ pkgs, hackGet, ... }: {
+  android.applicationId = "systems.obsidian.obelisk.examples.minimal";
+  android.displayName = "Obelisk Minimal Example";
+  ios.bundleIdentifier = "systems.obsidian.obelisk.examples.minimal";
+  ios.bundleName = "Obelisk Minimal Example";
+  overrides = pkgs.lib.composeExtensions (pkgs.callPackage (hackGet ./dep/rhyolite) {}).haskellOverrides
+    (self: super: with pkgs.haskell.lib; {
+      # Your custom overrides go here.
+    });
+})
